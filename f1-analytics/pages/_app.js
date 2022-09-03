d7097aab58424b7bc82bd2dcbb43da6ff934dd23
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2022-09-03 09:18:39 +0100

    initial commit

---
+++ f1-analytics/pages/_app.js
@@ -0,0 +1,8 @@
+const App = ({component}) => {
+    return (
+        <Head>
+            <title>F1 Analytics</title>
+            <meta name="viewport" content="width=device-width, initial-scale-1.0, maximum-scale=1.0, user-scalable=no"/>
+        </Head>
+    )
+}
